3796b7c79cbfe0931252759f0b2b29ec97ef9601
Demaris Ashmore <demiashmore@yahoo.com>
2023-03-02 17:16:38 -0700

    commit tree f2dea586176e38989df06f096a2b1d02b3794e4f

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,43 @@
 # your code here
+
+class Stack
+
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        raise 'Stack Overflow' if @limit && @stack.length == @limit
+        @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[@stack.length - 1]
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full?
+        @limit && @stack.length == @limit ? true : false
+    end
+
+    def search(value)
+        @stack.each_with_index do | item, index |
+            return size - index - 1 if item == value
+        end
+        -1
+    end
+
+end
